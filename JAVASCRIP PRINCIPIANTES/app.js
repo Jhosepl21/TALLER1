019d305c8b75f3468410ce48dcf789a8f37ff6c9
Jhosepl21 <2020101139@unfv.edu.pe>
2022-12-02 19:28:42 -0500

    aprendiendo

---
+++ JAVASCRIP PRINCIPIANTES/app.js
@@ -0,0 +1,20 @@
+count = 0
+while (count < 3)
+{
+  count = count + 1
+  console.log("Hola mundo")
+}
+
+let num1 = 6;
+let num2 = 8;
+let resultado = num1 + num2;
+console.log(" La suma de ",num1, "+ ",num2, " es = ",resultado);
+console.log("\n-------------------------") // Salto de linea
+
+const names = ['Aiden', 'Heather', 'Michael']
+console.log(names.length)
+for(let i=0;i<3;i++)
+{
+  console.log("El nombre numero "+ (i+1) + " es: " + names[i])
+}
+console.log(names)
