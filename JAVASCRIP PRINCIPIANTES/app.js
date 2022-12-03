fe1c232aedb83e0bd31f5425ef37f9b337772633
Jhosepl21 <2020101139@unfv.edu.pe>
2022-12-03 13:05:18 -0500

    3/12

--- JAVASCRIP PRINCIPIANTES/app.js
+++ JAVASCRIP PRINCIPIANTES/app.js
@@ -4,5 +4,24 @@ console.log("El area del circulo es "+radio*radio+"π");
  if(radio*radio>25){
     console.log("EL CIRCULO ES MUY GRANDE")
   }else{
-    console.log("muy chico")
+    console.log("muy chico")}
+  
+console.log("Numero fibonacci");
+var x=1;
+var a=0;
+var b=1;
+let n=prompt("INGRESE EL TAMAÑO DE NUMEROS GENERADOS");
+while(x<=n){
+  if(x%2==1){
+    letraA=a+"<br>";
+    document.write(letraA)
+    a=a+b
+  }else{
+    letraB=b+"<br>"
+    document.write(letraB)
+      b=b+a
+    
   }
+ x=x+1
+}
+  
